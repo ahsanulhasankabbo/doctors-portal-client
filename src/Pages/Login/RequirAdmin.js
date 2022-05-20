12d3275d0c48fafc5f29eb45adbff08a1318325f
Ahsanul Hasan Kabbo <kabboahsanulhasan@gmail.com>
2022-05-20 12:24:26 +0600

    requir admin error handle

--- src/Pages/Login/RequirAdmin.js
+++ src/Pages/Login/RequirAdmin.js
@@ -1,23 +1,25 @@
-import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
-import useAdmin from '../../hooks/useAdmin';
 import Loading from '../Shared/Loading';
+import useAdmin from '../../hooks/useAdmin';
+import { signOut } from 'firebase/auth';
 
-const RequirAdmin = ({children}) => {
+const RequireAdmin = ({children}) => {
     const [user, loading] = useAuthState(auth);
     const [admin, adminLoading] = useAdmin(user);
     const location = useLocation();
+
     if(loading || adminLoading){
         return <Loading></Loading>
     }
-    if(!user || admin){
+
+    if(!user || !admin){
         signOut(auth);
-        return <Navigate to="/login" state={{ from: location }} replace />;
+        return <Navigate to="/login" state={{ from: location }} replace></Navigate>
     }
     return children;
 };
 
-export default RequirAdmin;
+export default RequireAdmin;
